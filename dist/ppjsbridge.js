fb1996fb0c65d4d7bce35020ebf4a0cf94866e59
gongxiaoming <550843110@qq.com>
2020-03-12 20:34:35 +0800

    v0.1.0

--- dist/ppjsbridge.js
+++ dist/ppjsbridge.js
@@ -1,4 +1,4 @@
-/*!@apeiwan/ppjsbridge released@0.1.4*/
+/*!@apeiwan/ppjsbridge released@0.1.0*/
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
