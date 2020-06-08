7dc8eae895c20e3efbffd4f3717408395e978c3a
gongxiaoming <550843110@qq.com>
2020-06-08 16:12:52 +0800

    还原原来的更改

--- src/exports/getToken.js
+++ src/exports/getToken.js
@@ -1,7 +1,7 @@
 import {isPiPiApp} from '../utils/constant'
 
 let getToken = function () {
-  return isPiPiApp ? window.PPJSBridgeReady ? sessionStorage.getItem('token') : localStorage.getItem('token') : '';
+  return isPiPiApp ? sessionStorage.getItem('token') || '' : '';
 };
 
 export default getToken
