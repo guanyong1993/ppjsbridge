459fcf765641b455e8539cd36d7c4cc693594aa6
gongxiaoming <550843110@qq.com>
2021-06-15 15:52:33 +0800

    update output interface

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import invoke from './exports/invoke'
 import {isCanIUse} from './utils/canIUse'
-import {getStitchingUrlParams, getRequestUrlParam} from './utils/url'
+import {getStitchingUrlParams, getRequestUrlParam, addUrlParams} from './utils/url'
 import login from './exports/login'
 import ready from './exports/ready'
 import openWindow from './exports/openWindow'
 import getToken from "./exports/getToken";
 import getEnv from "./exports/getEnv";
+import init from "./exports/init";
 import getVersion from "./exports/getVersion";
 import isLogin from "./exports/isLogin";
+import {share, getFormatShareUrl} from "./exports/share";
 import getApp from "./exports/getApp";
 import {os, isPiPiApp} from "./utils/constant";
 import back from "./exports/back";
@@ -27,5 +29,9 @@ export default {
   isLogin,
   back,
   getStitchingUrlParams,
-  getRequestUrlParam
+  getRequestUrlParam,
+  addUrlParams,
+  share,
+  getFormatShareUrl,
+  init
 };
